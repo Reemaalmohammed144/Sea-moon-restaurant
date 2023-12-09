2f0b23c7ccb6ed32f42ac8b17449d2b596c95cf1
Reema <reema.almohammed@students.Plymouth.ac.uk>
2023-12-10 03:09:46 +0500

    login screen added

--- app/src/main/java/com/example/seamoonrestaurant/SplashActivity.java
+++ app/src/main/java/com/example/seamoonrestaurant/SplashActivity.java
@@ -2,7 +2,11 @@ package com.example.seamoonrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Button myButton = findViewById(R.id.loginBtn);
+
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
