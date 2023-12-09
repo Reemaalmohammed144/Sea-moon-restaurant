c67ff56b4927f686cfd0e031df02b23393e3de52
Reema <reema.almohammed@students.Plymouth.ac.uk>
2023-12-10 03:51:39 +0500

    sign up screen added

--- app/src/main/java/com/example/seamoonrestaurant/SignUpActivity.java
+++ app/src/main/java/com/example/seamoonrestaurant/SignUpActivity.java
@@ -2,25 +2,24 @@ package com.example.seamoonrestaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
-        TextView signUpText = findViewById(R.id.signUpText);
-        signUpText.setOnClickListener(new View.OnClickListener() {
+        TextView loginText = findViewById(R.id.loginText);
+        loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
