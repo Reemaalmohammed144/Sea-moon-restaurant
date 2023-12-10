278adead8124ddfefbd59af563e989cd5782e650
Reema <reema.almohammed@students.Plymouth.ac.uk>
2023-12-10 05:06:55 +0500

    menu screen added

--- app/src/main/java/com/example/seamoonrestaurant/LoginActivity.java
+++ app/src/main/java/com/example/seamoonrestaurant/LoginActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -31,6 +32,16 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent intent = new Intent(LoginActivity.this, EmailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button loginBtn = findViewById(R.id.loginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
                 startActivity(intent);
             }
         });
